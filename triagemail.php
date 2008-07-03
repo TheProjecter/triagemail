1312fb25dbef43c26c1695cb6c017bb9f10d6a23
brian.suda <brian.suda@96e83c53-1e51-0410-b04e-e16b8820d9d2>
2008-07-03 17:03:02 +0000

    created base PHP script with steps

---
+++ triagemail.php
@@ -0,0 +1,16 @@
+<?php
+// brian suda
+// triagemail.com
+// 2008-06-03
+
+// Step #1
+// display login
+
+// Step #2
+// fetch next email and request action
+
+// Step #3
+// deal with response and go to step #2 until empty
+
+
+?>
